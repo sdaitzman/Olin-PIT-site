0e0b07384fea303c0f8102f8eab790d220e8dce4
Sam Daitzman <sam@daitzman.com>
2019-12-25 16:11:58 -0500

    reverts to default layout for new approach

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,6 +1,5 @@
 module.exports = {
     title: 'PInT',
-    description: 'PInT is the Public Interest Tech group at Olin College.',
     themeConfig: {
         nav: [
             { text: 'Home', link: '/' },
---
+++ blog/test-post copy 10.md
@@ -0,0 +1,12 @@
+---
+title: Test Blog Post Title
+date: 2019-11-29
+description:
+    This is an article that was created as a test! Cool. PInT.
+---
+
+# Header
+
+Content
+
+This is an article! PInT @ Olin!
