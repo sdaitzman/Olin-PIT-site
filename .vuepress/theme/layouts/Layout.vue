b8f99396e94375087fe53818a3a154645ceac111
Sam Daitzman <sam@daitzman.com>
2019-12-25 20:33:40 -0500

    basic layout and font

--- .vuepress/theme/layouts/Layout.vue
+++ .vuepress/theme/layouts/Layout.vue
@@ -5,13 +5,39 @@
     </div>
 </template>
 
-<style lang="scss" scoped>
-.body-container {
-    background: pink;
+<style lang="css">
+body {
+  box-sizing: border-box;
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+::selection {
+  color: white;
+  background: #fbcf16;
+}
+html {
+  animation: fadeIn 0.4s ease-out;
+  font-family: "IBM Plex Sans";
+  font-weight: 300;
+  /* text-align: justify; */
 }
 </style>
 
+<style lang="scss" scoped>
+
+
+.body-container {
+  position: relative;
+  max-width: 850px;
+}
+
+</style>
+
 <script>
+import 'typeface-ibm-plex-sans'
 import Nav from "@theme/components/Nav";
 
 export default {
