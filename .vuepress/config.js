4754a280927d4906eadc5d5082d756bf33802945
Sam Daitzman <sam@daitzman.com>
2019-12-26 09:21:04 -0500

    sets up analytics

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -3,6 +3,14 @@ module.exports = {
     themeConfig: {
         logo: '/pint.png'
     },
+    plugins: [
+        [
+            '@vuepress/google-analytics',
+            {
+                'ga': 'UA-154156572-1' // UA-00000000-0
+            }
+        ]
+    ],
     'markdown': {
         anchor: {
             permalink: false,
