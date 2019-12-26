7c7b7582db3aeca24993c6f8c24a518f676d1352
Sam Daitzman <sam@daitzman.com>
2019-12-26 09:10:58 -0500

    removes nav from config

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,19 +1,6 @@
 module.exports = {
     title: 'PInT',
     themeConfig: {
-        nav: [{
-                text: 'Home',
-                link: '/'
-            },
-            {
-                text: 'About',
-                link: '/about/'
-            },
-            {
-                text: 'Blog',
-                link: '/blog/'
-            }
-        ],
         logo: '/pint.png'
     },
     'markdown': {
